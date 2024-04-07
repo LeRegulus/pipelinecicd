c1e79545612628c66c15cc53c2580f8da86c6ad1
Alassane Seck <alassane.seck@m2t.biz>
2024-04-07 15:58:35 +0000

    Initialisation

--- src/main/java/org/regulus/cicdintegration/web/TestController.java
+++ src/main/java/org/regulus/cicdintegration/web/TestController.java
@@ -1,2 +1,18 @@
-package org.regulus.cicdintegration.web;public class TestController {
+package org.regulus.cicdintegration.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<Map<String, String>> test(){
+        return ResponseEntity.ok().body(Map.of("Testing", "Up and Running"));
+    }
 }
